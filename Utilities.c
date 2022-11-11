56ee0844859c9277f649f1391f0db016e2b7e8f4
ana-mali <92402688+ana-mali@users.noreply.github.com>
2022-11-10 19:44:43 -0500

    Add files via upload

---
+++ Utilities.c
@@ -0,0 +1,33 @@
+//Anastasia, Jonotham and Ruicheng
+//CP460 Applied Cryptography Project
+
+//Module used for utility functions for RSA 
+//Classes can be used to form attributes such as:
+//Sender info, reciever info, sender/reciever public and private key
+
+/*
+Parameters:
+    plain - string as plain text
+Returns:
+    output - string as cipher text
+*/
+char encrypt(char *plain){
+
+}
+
+/*
+Parameters:
+    cipher - string as cipher text
+Returns:
+    output - string as plain text
+*/
+char decrupt(char *cipher){
+
+}
+
+/*
+Can generate either public or private key
+*/
+char create_key(){
+
+}
